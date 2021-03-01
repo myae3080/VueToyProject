39b8c134f92da43a9de6c6aba5b7ba2076161382
s <myae3080@naver.com>
2021-03-02 06:47:37 +0900

    modify

--- src/App.vue
+++ src/App.vue
@@ -6,51 +6,54 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+<!--        <v-img-->
+<!--          alt="Vuetify Logo"-->
+<!--          class="shrink mr-2"-->
+<!--          contain-->
+<!--          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"-->
+<!--          transition="scale-transition"-->
+<!--          width="40"-->
+<!--        />-->
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+<!--        <v-img-->
+<!--          alt="Vuetify Name"-->
+<!--          class="shrink mt-1 hidden-sm-and-down"-->
+<!--          contain-->
+<!--          min-width="100"-->
+<!--          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"-->
+<!--          width="100"-->
+<!--        />-->
+          Todo
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+<!--      <v-btn-->
+<!--        href="https://github.com/vuetifyjs/vuetify/releases/latest"-->
+<!--        target="_blank"-->
+<!--        text-->
+<!--      >-->
+        <span class="mr-2">Toy Project</span>
+<!--        <v-icon>mdi-open-in-new</v-icon>-->
+<!--      </v-btn>-->
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+<!--      <HelloWorld/>-->
+        <ToyTodoHeader/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
+import ToyTodoHeader from './components/ToyTodoHeader';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+      ToyTodoHeader,
   },
 
   data: () => ({
---
+++ src/components/ToyTodoHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-stepper-header>
+        <h1>Header</h1>
+    </v-stepper-header>
+</template>
+
+<script>
+export default {
+    name: "ToyTodoHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
